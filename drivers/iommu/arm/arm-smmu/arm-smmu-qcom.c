ac4ea80c8c37f584fabb927ffd2ee3e142bf4b14
AngeloGioacchino Del Regno <angelogioacchino.delregno@somainline.org>
2021-08-10 20:42:48 +0200

    iommu/arm-smmu-qcom: Add MSM8998 and SDM660 mss compatibles for identity
    Add MSM8998 and SDM660's mss-pil compatibles to switch the default
    iommu domain type to IDENTITY, as similarly required by SDM845 and
    others.
    
    Signed-off-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@somainline.org>

--- drivers/iommu/arm/arm-smmu/arm-smmu-qcom.c
+++ drivers/iommu/arm/arm-smmu/arm-smmu-qcom.c
@@ -232,10 +232,12 @@ static const struct of_device_id qcom_smmu_client_of_match[] __maybe_unused = {
 	{ .compatible = "qcom,adreno" },
 	{ .compatible = "qcom,mdp4" },
 	{ .compatible = "qcom,mdss" },
+	{ .compatible = "qcom,msm8998-mss-pil" },
 	{ .compatible = "qcom,sc7180-mdss" },
 	{ .compatible = "qcom,sc7180-mss-pil" },
 	{ .compatible = "qcom,sc7280-mdss" },
 	{ .compatible = "qcom,sc8180x-mdss" },
+	{ .compatible = "qcom,sdm660-mss-pil" },
 	{ .compatible = "qcom,sdm845-mdss" },
 	{ .compatible = "qcom,sdm845-mss-pil" },
 	{ }
